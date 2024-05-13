4595c4598a146c9449cf9a8bfc3c13565b5dd46f
David wu <wlwen1117@163.com>
2024-05-13 17:53:44 +0800

    移除元素更新

---
+++ 删除排序数组中的重复项/源.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		int FastIndex = 0;
+		int SlowIndex = 0;
+		int length = nums.size();
+
+		for (int i = 0; i < length; ++i)
+		{
+			int target = nums[i];
+			if (i - 1 >= 0 && nums[i - 1] == target) 
+			{
+				for (SlowIndex = i, FastIndex = i; FastIndex < length; ++FastIndex)
+				{
+					if (target != nums[FastIndex])
+					{
+						nums[SlowIndex++] = nums[FastIndex];
+					}
+				}
+				i--;
+				length--;
+			}
+			
+		}
+
+		return length;
+	}
+
+};
+
+
+int main(void)
+{
+	vector<int> nums = { 1, 1, 2};
+	Solution it;
+	int result = it.removeDuplicates(nums);
+	cout << result << endl;
+	for (auto it = nums.begin(); it != nums.end(); ++it)
+	{
+		cout << *it << endl;
+	}
+	//for (auto & it : nums)
+	//{
+	//	cout << it << " ";
+	//}
+	//cout << endl;
+	return 0;
+}
--- 移除元素/源.cpp
+++ 移除元素/源.cpp
@@ -8,24 +8,44 @@ using namespace std;
 class Solution {
 public:
 	int removeElement(vector<int>& nums, int val) {
-		int offset = 0;
 		int length = nums.size();
 		for (int i = 0; i < length; ++i)
 		{
 			if (nums[i] == val)
 			{
-				offset++;
 				for (int j = i + 1; j < length; ++j)
 				{
 					nums[j - 1] = nums[j];
 				}
-
+				i--;
+				length--;
 			}
-			i--;
+			
 		}
 		
 
-		return length - offset;
+		return length;
+
+	}
+
+};
+
+class Solution2 {
+public:
+	int removeElement(vector<int>& nums, int val) {
+		int length = nums.size();
+		int FastIndex = 0;
+		int SlowIndex = 0;
+		for (; FastIndex < length; ++FastIndex)
+		{
+			if (val != nums[FastIndex])
+			{
+				nums[SlowIndex++] = nums[FastIndex];
+			}
+		}
+
+
+		return SlowIndex;
 
 	}
 
@@ -35,7 +55,9 @@ int main(void)
 {
 	int val = 2;
 	vector<int> nums = { 0,1,2,2,3,0,4,2 };
-	Solution it;
+	//Solution it;
+	//cout << it.removeElement(nums, val) << endl;
+	Solution2 it;
 	cout << it.removeElement(nums, val) << endl;
 	for(auto it = nums.begin(); it != nums.end(); ++it)
 	{
